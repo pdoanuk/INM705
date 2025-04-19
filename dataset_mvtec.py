6e23129d89087377fcdb82d1717d95e6d0d6c585
Phuoc Dat Doan <Phuoc.Doan@city.ac.uk>
2025-04-19 18:57:51 +0100

    Added unified training

--- dataset_mvtec.py
+++ dataset_mvtec.py
@@ -8,19 +8,18 @@ import os
 import glob
 from PIL import Image
 import torch
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset, DataLoader, ConcatDataset
 from torchvision import transforms
 import numpy as np
 from config import CLASS_NAMES, mean_train, std_train
 from pkg_resources import parse_version
 
-
-if parse_version(Image.__version__)>=parse_version('10.0.0'):
-    Image.ANTIALIAS=Image.LANCZOS
+if parse_version(Image.__version__) >= parse_version('10.0.0'):
+    Image.ANTIALIAS = Image.LANCZOS
 
 
 class MVTecDataset(Dataset):
-    def __init__(self, args, dataset_path, class_name, is_train=True, resize=384, transform_x=transforms.ToTensor()):
+    def __init__(self, args, dataset_path, class_name, is_train=True, resize=384, transform_x=None):
         super(MVTecDataset, self).__init__()
         assert class_name in CLASS_NAMES, 'class_name: {}, should be in {}'.format(class_name, CLASS_NAMES)
         self.args = args
@@ -28,20 +27,27 @@ class MVTecDataset(Dataset):
         self.class_name = class_name
         self.is_train = is_train
         self.resize = resize + self.args.image_size // 12
-        self.transform_x = transform_x
+        if transform_x is not None:
+            self.transform_x = transform_x
+        else:
+            self.transform_x = transforms.Compose(
+                [transforms.Resize(args.image_size, Image.ANTIALIAS),
+                 transforms.ToTensor(),
+                 transforms.Normalize(mean=mean_train, std=std_train),
+                 ])
 
         # load dataset
         self.x, self.y, self.mask = self.load_dataset_folder()
 
         self.transform_mask = transforms.Compose([transforms.Resize(self.args.image_size, Image.NEAREST),
-                                         transforms.ToTensor()])
+                                                  transforms.ToTensor()])
 
     def __getitem__(self, idx):
         x, y, mask = self.x[idx], self.y[idx], self.mask[idx]
         x = Image.open(x).convert('RGB')
         x = self.transform_x(x)
 
-        if mask == None:
+        if mask is None:
             mask = torch.zeros([1, self.args.image_size, self.args.image_size])
         else:
             mask = Image.open(mask)
@@ -60,7 +66,6 @@ class MVTecDataset(Dataset):
         img_types = sorted(os.listdir(img_dir))
 
         for img_type in img_types:
-
             # load images
             img_type_dir = os.path.join(img_dir, img_type)
             if not os.path.isdir(img_type_dir):
@@ -87,53 +92,43 @@ class MVTecDataset(Dataset):
         return list(x), list(y), list(mask)
 
 
-
-
 def get_dataloader(args):
-    obj_mean = mean_train
-    obj_std = std_train
-    trainT = transforms.Compose(
-        [
-            transforms.Resize(args.image_size, Image.ANTIALIAS),
-            transforms.ToTensor(),
-            transforms.Normalize(mean=obj_mean, std=obj_std),
-        ]
-    )
+    transform_func = transforms.Compose([transforms.Resize(args.image_size, Image.ANTIALIAS),
+                                         transforms.ToTensor(),
+                                         transforms.Normalize(mean=mean_train, std=std_train),
+                                         ])
+    #
+    # validT = transforms.Compose(
+    #     [
+    #         transforms.Resize(args.image_size, Image.ANTIALIAS),
+    #         transforms.ToTensor(),
+    #         transforms.Normalize(mean=obj_mean, std=obj_std),
+    #     ]
+    # )
+    #
+    # testT = transforms.Compose(
+    #     [
+    #         transforms.Resize(args.image_size, Image.ANTIALIAS),
+    #         transforms.ToTensor(),
+    #         transforms.Normalize(mean=obj_mean, std=obj_std),
+    #     ]
+    # )
 
-    validT = transforms.Compose(
-        [
-            transforms.Resize(args.image_size, Image.ANTIALIAS),
-            transforms.ToTensor(),
-            transforms.Normalize(mean=obj_mean, std=obj_std),
-        ]
-    )
-
-    testT = transforms.Compose(
-        [
-            transforms.Resize(args.image_size, Image.ANTIALIAS),
-            transforms.ToTensor(),
-            transforms.Normalize(mean=obj_mean, std=obj_std),
-        ]
-    )
-
-    train_dataset = MVTecDataset(
-        args,
-        args.dataset_path,
-        class_name=args.obj,
-        is_train=True,
-        resize=args.image_size,
-        transform_x=trainT,
-    )
-    valid_dataset = MVTecDataset(
-        args,
-        args.dataset_path,
-        class_name=args.obj,
-        is_train=True,
-        resize=args.image_size,
-        transform_x=validT,
-    )
+    train_dataset = MVTecDataset(args,
+                                 args.dataset_path,
+                                 class_name=args.obj,
+                                 is_train=True,
+                                 resize=args.image_size,
+                                 transform_x=transform_func
+                                 )
+    valid_dataset = MVTecDataset(args,
+                                 args.dataset_path,
+                                 class_name=args.obj,
+                                 is_train=True,
+                                 resize=args.image_size,
+                                 transform_x=transform_func,
+                                 )
     img_nums = len(train_dataset)
-
     indices = list(range(img_nums))
     np.random.seed(args.seed)
     np.random.shuffle(indices)
@@ -142,22 +137,14 @@ def get_dataloader(args):
     train_sampler = torch.utils.data.SubsetRandomSampler(train_idx)
     valid_sampler = torch.utils.data.SubsetRandomSampler(valid_idx)
 
-    test_dataset = MVTecDataset(
-        args,
-        args.dataset_path,
-        class_name=args.obj,
-        is_train=False,
-        resize=args.image_size,
-        transform_x=testT,
-    )
+    test_dataset = MVTecDataset(args,
+                                args.dataset_path,
+                                class_name=args.obj,
+                                is_train=False,
+                                resize=args.image_size,
+                                transform_x=transform_func,
+                                )
 
-    # train_loader = torch.utils.data.DataLoader(
-    #     train_dataset, batch_size=args.batch_size, sampler=train_sampler
-    # )
-    # val_loader = torch.utils.data.DataLoader(
-    #     valid_dataset, batch_size=args.batch_size, sampler=valid_sampler
-    # )
-    # test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=1, shuffle=False)
     train_loader = DataLoader(train_dataset,
                               batch_size=args.batch_size,
                               shuffle=True,
@@ -175,3 +162,65 @@ def get_dataloader(args):
                              pin_memory=True)
 
     return train_loader, val_loader, test_loader
+
+def get_loader_full(args, list_categories=None):
+    val_ratio = args.val_ratio
+    train_ratio = 1 - val_ratio
+
+    transform_func = transforms.Compose([transforms.Resize(args.image_size, Image.ANTIALIAS),
+                                         transforms.ToTensor(),
+                                         transforms.Normalize(mean=mean_train, std=std_train),
+                                         ])
+    if list_categories is None:
+        list_categories = CLASS_NAMES
+
+    train_datasets_list = []
+    val_datasets_list = []
+    test_datasets_list = []
+    for category in list_categories:
+        train_val_dataset = MVTecDataset(args,
+                                     args.dataset_path,
+                                     class_name=category,
+                                     is_train=True,
+                                     resize=args.image_size,
+                                     transform_x=transform_func
+                                     )
+        train_dataset, val_dataset = torch.utils.data.random_split(train_val_dataset,
+                                                                   [train_ratio, val_ratio])
+        train_datasets_list.append(train_dataset)
+        val_datasets_list.append(val_dataset)
+
+        test_dataset = MVTecDataset(args,
+                                     args.dataset_path,
+                                     class_name=category,
+                                     is_train=True,
+                                     resize=args.image_size,
+                                     transform_x=transform_func
+                                     )
+        test_datasets_list.append(test_dataset)
+
+    train_dataset_full = ConcatDataset(train_datasets_list)
+    val_dataset_full = ConcatDataset(val_datasets_list)
+    train_loader_full = DataLoader(train_dataset_full,
+                                   batch_size=args.batch_size,
+                                   shuffle=True,
+                                   num_workers=4,
+                                   pin_memory=True,
+                                   drop_last=True)
+    val_loader_full = DataLoader(val_dataset_full,
+                                   batch_size=args.batch_size,
+                                   shuffle=True,
+                                   num_workers=4,
+                                   pin_memory=True,
+                                   drop_last=True)
+    test_dataset_full = ConcatDataset(test_datasets_list)
+    test_loader_full = DataLoader(test_dataset_full,
+                                  batch_size=args.batch_size,
+                                  shuffle=False,
+                                  num_workers=4,
+                                  pin_memory=True,
+                                  drop_last=True)
+
+    return train_loader_full, val_loader_full, test_loader_full
+
+
